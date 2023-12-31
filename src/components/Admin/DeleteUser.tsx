b268182a72907f6a35d5314691af47c9ebca1c6e
dinhhuong2002 <dinhthihuongdung2002@gmail.com>
2023-12-31 21:33:33 +0700

    add: delateUser and searchUser

--- src/components/Admin/DeleteUser.tsx
+++ src/components/Admin/DeleteUser.tsx
@@ -1,38 +1,54 @@
-import useUser from "@/hooks/useUser";
-import Input from "../Input";
-import { ChangeEvent, useState } from "react";
-import SearchButton from "../ButtonSearch";
+'use client'
+import React, { useState } from 'react';
+import AdminPage from "./AdminPage";
 
-const SearchUser = () =>{
-    const [Search, setSearch] = useState('')
+const h1_style = {
+    color: 'white',
+    fontFamily: 'Dancing Script, cursive, Lobster, sans-serif, Arial',
+    display: 'flex',
+    fontSize: '2.0rem',
+    marginRight: '200px',
+};
 
-    const {data: users =[], isLoading,error,mutate} = useUser(Search)
-
-    const handleSearch = (event: ChangeEvent<HTMLInputElement>) =>{
-        setSearch(event.target.value)
-    }
+const button_style = {
     
+        backgroundColor: '#5cb85c',
+        color: '#fff',
+        marginLeft: '50px',
+        
+      
+}
+
+const DeleteUser = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleInputChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
+      setSearchTerm(e.target.value);
+    };
+  
+    const handleSearch = () => {
+      // Xử lý tìm kiếm, có thể điều hướng hoặc thực hiện logic tìm kiếm ở đây
+      console.log('Searching for:', searchTerm);
+    };
 
     return(
-        <>
-        <Input
-        type="text"
-        placeholder="Search User"
-        value={Search}
-        onChange={handleSearch}
-        label="Seacrh User"
-        />
-        <SearchButton/>
-        {users.map((user: Record<string, any>) => (
-            <div key={user.id} className="flex flex-row gap-4">
-            <div className="flex flex-col">
-                <p className="text-white font-semibold text-sm">{user.username}</p>
-                <p className="text-neutral-400 text-sm">@{user.id}</p>
+    <>
+        <section>
+            <AdminPage />
+        </section>
+        <section>
+            <h1 style={h1_style}>Nhập ID người dùng cần xóa: </h1>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={searchTerm}
+                    onChange={handleInputChange}
+                />
+                <button style={button_style}  onClick={handleSearch}>Search</button>
             </div>
-            </div>
-        ))}        
-        </>
+        </section>
+    </>
     )
-
 }
-export default SearchUser
+export default DeleteUser
